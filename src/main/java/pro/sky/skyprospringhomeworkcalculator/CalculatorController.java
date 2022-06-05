7c85d62e3dbae54a973f704c4f3566b5a7165464
porwa <o-la-la-repka@yandex.ru>
2022-06-05 13:56:09 +0300

    Complete

--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorController.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorController.java
@@ -10,4 +10,60 @@ import java.util.Objects;
 @RequestMapping("/calculator")
 @RestController
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping()
+    public String meets(){
+        return calculatorService.meets();
+    }
+    @GetMapping("/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Float a,
+                       @RequestParam(value = "num2", required = false) Float b){
+        if(Objects.isNull(a)||Objects.isNull(b)){
+            return "ne pravilnie parametri";
+        }
+        return buildStr(a,b,calculatorService.plus(a,b),"+");
+    }
+    @GetMapping("/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Float a,
+                        @RequestParam(value = "num2", required = false) Float b){
+        if(Objects.isNull(a)||Objects.isNull(b)){
+            return "ne pravilnie parametri";
+        }
+        return buildStr(a,b,calculatorService.minus(a,b),"-");
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Float a,
+                           @RequestParam(value = "num2", required = false) Float b){
+        if(Objects.isNull(a)||Objects.isNull(b)){
+            return "ne pravilnie parametri";
+        }
+        return buildStr(a,b,calculatorService.multiply(a,b),"*");
+    }
+    @GetMapping("/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Float a,
+                         @RequestParam(value = "num2", required = false) Float b){
+        if(Objects.isNull(a)||Objects.isNull(b)){
+            return "ne pravilnie parametri";
+        }
+        if(b==0) {
+            return "delit nelzya";
+        }else{
+            return buildStr(a,b,calculatorService.divide(a,b),"/");
+        }
+
+    }
+
+    private String buildStr(float a,
+                            float b,
+                            float result,
+                            String operation){
+        return String.format("%.1f%s%.1f=%.1f",a,operation,b,result);
+    }
+
 }
--- src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringhomeworkcalculator/CalculatorService.java
@@ -4,4 +4,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    public String meets(){
+        return "privCalc";
+    }
+
+    public float plus(float a, float b){
+        return a+b;
+    }
+
+    public float minus(float a, float b){
+        return a-b;
+    }
+
+    public float multiply(float a, float b){
+        return a*b;
+    }
+
+    public float divide(float a, float b){
+        return a/b;
+    }
+
 }
